a3a762597b721fac7bf124340e1e535f677259a5
Mark Kopenga <mkopenga@gmail.com>
2018-03-05 15:29:10 +0100

    Added the matrix product function to matrix.js

--- ml/matrix.js
+++ ml/matrix.js
@@ -6,20 +6,36 @@ class Matrix {
     this.colls = colls
     this.matrix = Array(rows).fill([]).map(i => Array(colls).fill(0)) // create the matrix and fill it with nulls
   }
-  multiply(n) {
+  multiply(n, toDo) {
+    // set toDo to true to force normal multiply
+    // set toDo to false to force a matrix product
     // n can be a number or matrix array
-    this.mathWrapper(n, 'multiply')
+    if ((typeof n == 'object' && n.length != this.matrix.length && typeof toDo == 'undefined') || (typeof toDo == 'boolean' && toDo === false)) {
+      return Array(n[0].length)
+      .fill(Array(this.matrix.length).fill(0))
+      .map((el, index1) =>
+        el.map((el, index2) =>
+          this.matrix[index1].reduce((acc, el, i) =>
+            acc + (el * n[i][index2])
+          ,0)
+        )
+      )
+    } else {
+      this.mathWrapper(n, 'multiply')
+      return this.matrix
+    }
   }
   add(n) {
     // n can be a number or matrix array
     this.mathWrapper(n, 'add')
+    return this.matrix
   }
   mathWrapper(n, toDo) {
     let wrapper = (h, g) =>
       (toDo == 'add') ?
         h + g :
         h * g
-    this.matrix = this.matrix.map((el, index) =>
+    return this.matrix = this.matrix.map((el, index) =>
       el.map((i, index2) =>
         wrapper(i,
           (typeof n == 'number') ?
